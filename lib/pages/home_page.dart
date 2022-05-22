ecf8483d841f487626fd11aa334cf618484a5277
Noel <https://github.com/pseudononameous/>
2022-05-23 02:23:44 +0800

    noel commit 1

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -41,7 +41,7 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
       ),
 
       bottomNavigationBar: Material(
-        color: Colors.blue,
+        color: Colors.green,
         child: new TabBar(
           controller: _controller,
             tabs:<Widget>[
