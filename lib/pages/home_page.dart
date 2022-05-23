64666867f3be79f52308a66d5a02c1470f8d068d
Noel <noelchrishidalgo.hernandez@bicol-u.edu.ph>
2022-05-23 11:05:25 +0800

    commit ko sa android studio

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -125,7 +125,7 @@ class _HomePageState extends State<HomePage>
                 Container(
                   height: 100,
                   width: 100,
-                  color: Colors.amber,
+                  color: Colors.blue,
                 ),
                 SizedBox(
                   width: 20,
