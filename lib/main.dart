35831f8b5c227c9805dd7ae9b41ca4cf8844a127
Noel <noelchrishidalgo.hernandez@bicol-u.edu.ph>
2022-05-23 19:06:35 +0800

    registration module

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:emedicare/registration.dart';
 import 'package:flutter/material.dart';
 import 'package:emedicare/pages/home_page.dart';
 import 'login_page.dart';
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: RegisterPage(),
     );
   }
 }
---
+++ lib/registration.dart
@@ -0,0 +1,207 @@
+import 'package:flutter/material.dart';
+import 'package:emedicare/widgets/custom_checkbox.dart';
+import 'package:emedicare/widgets/primary_button.dart';
+import 'theme.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  bool passwordVisible = false;
+  bool passwordConfrimationVisible = false;
+  void togglePassword() {
+    setState(() {
+      passwordVisible = !passwordVisible;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(24.0, 100.0, 24.0, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Create new\nAccount',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40,
+                    )
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                ],
+              ),
+
+              Padding(
+                padding: EdgeInsets.only(left: 5.0),
+                child: Row(
+                  //mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Already have an account? ",
+                      style: regular16pt.copyWith(color: textGrey),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pop(
+                          context,
+                        );
+                      },
+                      child: Text(
+                        'Log in here',
+                        style: regular16pt.copyWith(color: primaryBlue),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(
+                height: 32,
+              ),
+              Form(
+                child: Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: textWhiteGrey,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.email,
+                            size: 21,
+                          ),
+                          hintText: 'Email',
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: textWhiteGrey,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: TextFormField(
+                        obscureText: !passwordVisible,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.lock,
+                            size: 21,
+                          ),
+                          hintText: 'Password',
+                          suffixIcon: IconButton(
+                            color: textGrey,
+                            splashRadius: 1,
+                            icon: Icon(passwordVisible
+                                ? Icons.visibility_outlined
+                                : Icons.visibility_off_outlined),
+                            onPressed: togglePassword,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: textWhiteGrey,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: TextFormField(
+                        obscureText: !passwordConfrimationVisible,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.lock,
+                            size: 21,
+                          ),
+                          hintText: 'Confirm Password',
+                          suffixIcon: IconButton(
+                            color: textGrey,
+                            splashRadius: 1,
+                            icon: Icon(passwordConfrimationVisible
+                                ? Icons.visibility_outlined
+                                : Icons.visibility_off_outlined),
+                            onPressed: () {
+                              setState(() {
+                                passwordConfrimationVisible =
+                                !passwordConfrimationVisible;
+                              });
+                            },
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 32,
+              ),
+
+              CustomPrimaryButton(
+                buttonColor: primaryBlue,
+                textValue: 'Sign Up',
+                textColor: Colors.white,
+              ),
+              SizedBox(
+                height: 25,
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CustomCheckbox(),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'By creating an account, you agree to our',
+                        style: regular16pt.copyWith(color: textGrey),
+                      ),
+                      Text(
+                        'Terms & Conditions',
+                        style: regular16pt.copyWith(color: primaryBlue),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
