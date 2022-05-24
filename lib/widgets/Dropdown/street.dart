f69e795f9a7d7afa5c0be74bd446785b1e33e380
kian <christiankennethnarvaez.reotan@bicol-u.edu.ph>
2022-05-24 14:25:34 +0800

    Moved to new folder Dropdown

---
+++ lib/widgets/Dropdown/street.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
+
+class Street extends StatefulWidget {
+  @override
+  _StreetState createState() => _StreetState();
+}
+
+class _StreetState extends State<Street> {
+  List<String> genderitems = ['A1', 'B2', 'C3'];
+  String? selectedvalue;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonFormField2(
+      decoration: InputDecoration(
+        //Add isDense true and zero Padding.
+        //Add Horizontal padding using buttonPadding and Vertical padding by increasing buttonHeight instead of add Padding here so that The whole TextField Button become clickable, and also the dropdown menu open under The whole TextField Button.
+        isDense: true,
+        contentPadding: EdgeInsets.zero,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(50),
+        ),
+        //Add more decoration as you want here
+        //Add label If you want but add hint outside the decoration to be aligned in the button perfectly.
+      ),
+      isExpanded: true,
+      hint: const Text(
+        'Street',
+        style: TextStyle(fontSize: 16),
+      ),
+      icon: const Icon(
+        Icons.arrow_drop_down,
+        color: Colors.black45,
+      ),
+      iconSize: 30,
+      buttonHeight: 60,
+      buttonPadding: const EdgeInsets.only(left: 27, right: 10),
+      dropdownDecoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      items: genderitems
+          .map((item) => DropdownMenuItem<String>(
+                value: item,
+                child: Text(
+                  item,
+                  style: const TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ))
+          .toList(),
+      validator: (value) {
+        if (value == null) {
+          return 'Street';
+        }
+      },
+      onChanged: (value) {
+        //Do something when changing the item if you want.
+      },
+      onSaved: (value) {
+        selectedvalue = value.toString();
+      },
+    );
+  }
+}
